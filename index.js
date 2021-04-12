e7093bbe568540f856658796b2d79dd0eb01d998
José Hernández <jhernanq@everis.com>
2021-04-12 13:55:01 -0400

    test

--- index.js
+++ index.js
@@ -23,78 +23,90 @@ app.get("/", (req, res) => {
 });
 // all customers
 app.get('/users', (req, res) => {
-    const sql = 'SELECT * FROM user';
-  
-    connection.query(sql, (error, results) => {
-      if (error) throw error;
-      if (results.length > 0) {
-        res.json(results);
-      } else {
-        res.send('Not result');
-      }
-    });
+  const sql = 'SELECT * FROM user';
+
+  connection.query(sql, (error, results) => {
+    if (error) {
+      res.send('Not result, Error, app.get');
+    }
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send('Not result');
+    }
   });
-  
-  app.get('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const sql = `SELECT * FROM user WHERE id = ${id}`;
-    connection.query(sql, (error, result) => {
-      if (error) throw error;
-  
-      if (result.length > 0) {
-        res.json(result);
-      } else {
-        res.send('Not result');
-      }
-    });
-  });
-  
-  app.post('/add', (req, res) => {
-    const sql = 'INSERT INTO user SET ?';
-  
-    const customerObj = {
-      name: req.body.name,
-      city: req.body.city
-    };
-  
-    connection.query(sql, customerObj, error => {
-      if (error) throw error;
-      res.send('Customer created!');
-    });
-  });
-  
-  app.put('/update/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, city } = req.body;
-    const sql = `UPDATE user SET name = '${name}', city='${city}' WHERE id =${id}`;
-  
-    connection.query(sql, error => {
-      if (error) throw error;
-      res.send('Customer updated!');
-    });
-  });
-  
-  app.delete('/delete/:id', (req, res) => {
-    const { id } = req.params;
-    const sql = `DELETE FROM user WHERE id= ${id}`;
-  
-    connection.query(sql, error => {
-      if (error) throw error;
-      res.send('Delete customer');
-    });
-  });
-  
-  // Check connect
-  connection.connect(error => {
-    if (error) throw error;
-    console.log('Database server running!');
-  });
-  
-  connection.on('uncaughtException', function(err) {
-    logger.log('error', 'Fatal uncaught exception crashed cluster', err, function(err, level, msg, meta) {
-      console.log(`Server running on port ${PORT}`)
-      connection.exit(1);
-    });
 });
 
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `SELECT * FROM user WHERE id = ${id}`;
+  connection.query(sql, (error, result) => {
+    if (error) {
+      res.send('Not result, Error, app.get-ID');
+    }
+
+    if (result.length > 0) {
+      res.json(result);
+    } else {
+      res.send('Not result');
+    }
+  });
+});
+
+app.post('/add', (req, res) => {
+  const sql = 'INSERT INTO user SET ?';
+
+  const customerObj = {
+    name: req.body.name,
+    city: req.body.city
+  };
+
+  connection.query(sql, customerObj, error => {
+    if (error) {
+      res.send('Not result, Error, app.post');
+    }
+    res.send('Customer created!');
+  });
+});
+
+app.put('/update/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, city } = req.body;
+  const sql = `UPDATE user SET name = '${name}', city='${city}' WHERE id =${id}`;
+
+  connection.query(sql, error => {
+    if (error) {
+      res.send('Not result, Error, app.put');
+    }
+    res.send('Customer updated!');
+  });
+});
+
+app.delete('/delete/:id', (req, res) => {
+  const { id } = req.params;
+  const sql = `DELETE FROM user WHERE id= ${id}`;
+
+  connection.query(sql, error => {
+    if (error) {
+      res.send('Not result, Error, app.delete');
+    }
+    res.send('Delete customer');
+  });
+});
+
+// Check connect
+connection.connect(error => {
+  if (error) {
+    res.send('Not result, Error, connection.connect');
+  }
+  console.log('Database server running!');
+});
+
+connection.on('uncaughtException', function (err) {
+  logger.log('error', 'Fatal uncaught exception crashed cluster', err, function (err, level, msg, meta) {
+    console.log(`Server running on port ${PORT}`)
+    connection.exit(1);
+  });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
